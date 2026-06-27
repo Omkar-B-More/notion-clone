5223cef0312de46223f09765005b777e3810243a
Omkar-B-More <omkarmore8103@gmail.com>
2026-06-27 22:18:39 +0530

    Added layout.tsx to Notion clone history

---
+++ app/layout.tsx
@@ -0,0 +1,52 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Notion",
+  description: "The connected workspace where better, faster work happens.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.svg",
+        href: "/logo.svg",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo-dark.svg",
+        href: "/logo-dark.svg",
+      },
+    ],
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en" suppressHydrationWarning
+      className={`${inter.className} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">
+        <ThemeProvider 
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+        storageKey="notion-theme"
+        >
+        {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
